de29bd6b33f9777eff6dad53376fc8fe4b5e4556
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-21 01:40:37 +0100

    Add JoltPhysics dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include <Jolt/Jolt.h>
+
+#include <Jolt/Core/Memory.h>
+
 #include "game.hpp"
 
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 600;
 
 int main(int argc, char **argv) {
+  JPH::RegisterDefaultAllocator();
   pdx::Game game("Paradox", SCREEN_WIDTH, SCREEN_HEIGHT);
   game.Run();
   return 0;

a89d2945dfd059792539c60a4bea1fdd9c3313e7
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-22 21:56:58 +0100

    Remove Physics invocation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include "game.hpp"
-#include "physics.hpp"
 
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 600;
 
 int main(int argc, char **argv) {
-  pdx::PhysicsHandle handle = pdx::GamePhysics::GetPhysicsHandle();
   pdx::Game game("Paradox", SCREEN_WIDTH, SCREEN_HEIGHT);
   game.Run();
   return 0;

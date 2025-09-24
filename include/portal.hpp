d4473df9782fa59961fd8a16681f59be03a21a15
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-24 02:04:20 +0100

    Add Portal Class

--- include/portal.hpp
+++ include/portal.hpp
@@ -2,15 +2,34 @@
 #define __HPP_PARADOX_PORTAL__
 
 #include "camera.hpp"
+#include "shader.hpp"
+#include <memory>
 
 namespace pdx {
 class Portal {
 public:
-  Portal(const glm::vec3& position, const glm::vec3& up,
-         const glm::vec3& front);
+  Portal(const pdx::Camera& viewPoint, float rotation);
+
+  auto Position() const -> glm::vec3;
+  auto Front() const -> glm::vec3;
+  auto ModelMatrix() const -> glm::mat4;
+  auto ViewMatrix() const -> glm::mat4;
+  auto GetCamera() const -> pdx::Camera;
+
+  auto DrawPortalFrame(const glm::mat4& view, const glm::mat4& proj,
+                       const pdx::Shader& shader) const -> void;
+  auto DrawPortalPlane(const glm::mat4& view, const glm::mat4& proj,
+                       const pdx::Shader& shader) const -> void;
+
+  auto ClippedProj(const glm::mat4& view, const glm::mat4& proj) const
+      -> glm::mat4;
+
+  auto SetDestination(pdx::Portal *portal) -> void;
+  auto GetDestination() const -> pdx::Portal *;
 
 private:
   pdx::Camera m_Viewpoint;
+  pdx::Portal *m_Destination;
   glm::mat4 m_ModelMatrix;
 };
 } // namespace pdx
--- src/portal.cpp
+++ src/portal.cpp
@@ -3,6 +3,7 @@
 #include "model.hpp"
 
 #include <glm/ext/matrix_transform.hpp>
+#include <glm/gtc/matrix_access.hpp>
 
 using namespace pdx;
 
@@ -11,15 +12,77 @@ static AssetDir portalFrameDir{"data", "models", "portalFrame"};
 static std::optional<pdx::Model> portalPlane = {};
 static std::optional<pdx::Model> portalFrame = {};
 
-Portal::Portal(const glm::vec3& position, const glm::vec3& up,
-               const glm::vec3& front)
-    : m_Viewpoint(Camera(position, up, front)) {
+Portal::Portal(const pdx::Camera& viewpoint, float rotation)
+    : m_Viewpoint(viewpoint) {
   if (!portalPlane.has_value()) {
     portalPlane = pdx::Model::FromGLTF(portalPlaneDir.GetFile("scene.gltf"));
   }
   if (!portalFrame.has_value()) {
     portalFrame = pdx::Model::FromGLTF(portalFrameDir.GetFile("scene.gltf"));
   }
-  m_ModelMatrix = m_Viewpoint.GetRotationMatrix();
-  m_ModelMatrix = glm::translate(m_ModelMatrix, position);
+  m_ModelMatrix = glm::mat4(1.0);
+  m_ModelMatrix = glm::translate(m_ModelMatrix, m_Viewpoint.Position());
+  m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(90.0f),
+                              glm::vec3(1.0f, 0.0f, 0.0f));
+  m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(rotation),
+                              glm::vec3(0.0f, 0.0f, 1.0f));
 }
+
+auto Portal::DrawPortalFrame(const glm::mat4& view, const glm::mat4& proj,
+                             const pdx::Shader& shader) const -> void {
+  if (portalFrame.has_value()) {
+    shader.Use();
+    shader.SetMat4fv("model", m_ModelMatrix);
+    shader.SetMat4fv("view", view);
+    shader.SetMat4fv("projection", proj);
+    portalFrame->Draw();
+  }
+}
+
+auto Portal::DrawPortalPlane(const glm::mat4& view, const glm::mat4& proj,
+                             const pdx::Shader& shader) const -> void {
+  if (portalPlane.has_value()) {
+    shader.Use();
+    shader.SetMat4fv("model",
+                     glm::scale(m_ModelMatrix, glm::vec3(2.0f, 2.0f, 2.0f)));
+    shader.SetMat4fv("view", view);
+    shader.SetMat4fv("projection", proj);
+    portalPlane->Draw();
+  }
+}
+
+auto Portal::Position() const -> glm::vec3 { return m_Viewpoint.Position(); }
+auto Portal::Front() const -> glm::vec3 { return m_Viewpoint.Front(); }
+auto Portal::ModelMatrix() const -> glm::mat4 { return m_ModelMatrix; }
+auto Portal::ViewMatrix() const -> glm::mat4 {
+  return m_Viewpoint.GetViewMatrix();
+}
+auto Portal::GetCamera() const -> pdx::Camera { return m_Viewpoint; }
+
+auto Portal::ClippedProj(const glm::mat4& view, const glm::mat4& proj) const
+    -> glm::mat4 {
+  float dist = glm::length(Position());
+  glm::vec4 clipPlane(Front(), dist);
+  clipPlane = glm::inverse(glm::transpose(view)) * clipPlane;
+
+  if (clipPlane.w > 0.0f)
+    return proj;
+
+  glm::vec4 q =
+      glm::inverse(proj) *
+      glm::vec4(glm::sign(clipPlane.x), glm::sign(clipPlane.y), 1.0f, 1.0f);
+
+  glm::vec4 c = clipPlane * (2.0f / (glm::dot(clipPlane, q)));
+
+  glm::mat4 newProj = proj;
+  // third row = clip plane - fourth row
+  newProj = glm::row(newProj, 2, c - glm::row(newProj, 3));
+
+  return newProj;
+}
+
+auto Portal::SetDestination(pdx::Portal *portal) -> void {
+  m_Destination = portal;
+}
+
+auto Portal::GetDestination() const -> pdx::Portal * { return m_Destination; }
